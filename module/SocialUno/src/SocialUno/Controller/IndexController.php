a52443ccf489e870f58dfd60cac6ab1c26cf0a3f
Diago <diago@agog.com.br>
2015-04-27 21:24:54 -0300

    paginacao

--- module/SocialUno/src/SocialUno/Controller/IndexController.php
+++ module/SocialUno/src/SocialUno/Controller/IndexController.php
@@ -13,24 +13,21 @@ class IndexController extends AbstractActionController
     public function indexAction()
     {
         $session = $this->getServiceLocator()->get('Session');
-        
-        
-       
-        
+            
         if (!$session->offsetGet('user')) {
 
             return $this->redirect()->toUrl('/social-uno/login/index');                       
         }
         
-//        $paginator = new Paginator(
-//                    new DoctrinePaginator(
-//                            new ORMPaginator($query)
-//                    )                
-//                );
-//        
-//        $paginator
-//                 ->setCurrentPageNumber($this->params()->fromRoute('page'))
-//                ->setItemCountPerPage(1);
+       $paginator = new Paginator(
+                   new DoctrinePaginator(
+                           new ORMPaginator($query)
+                   )                
+               );
+       
+       $paginator
+                ->setCurrentPageNumber($this->params()->fromRoute('page'))
+               ->setItemCountPerPage(1);
                
         return new ViewModel(
                 ['usuarios' => $paginator]
