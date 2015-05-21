726fdb1b02d82f72c989d62f5d9f97a27d00b76f
Diago <diago@agog.com.br>
2015-05-21 19:28:43 -0300

    novo modulo

---
+++ module/Main/src/Main/Controller/IndexController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Main\Controller;
+
+use SocialUno\Controller\ActionController;
+use Zend\View\Model\ViewModel;
+use DoctrineORMModule\Paginator\Adapter\DoctrinePaginator;
+use Doctrine\ORM\Tools\Pagination\Paginator as ORMPaginator;
+use Zend\Paginator\Paginator;
+
+class IndexController extends ActionController
+{
+    public function indexAction()
+    {
+      
+       // $session = $this->getServiceLocator()->get('Session');
+           
+//        if (!$session->offsetGet('user'))
+//            return $this->redirect()->toUrl('/social-uno/login/index');                        
+               
+        
+       // $paginator = new Paginator(
+       //             new DoctrinePaginator(
+       //                     new ORMPaginator($query)
+       //             )                
+       //         );
+       
+       // $paginator
+       //          ->setCurrentPageNumber($this->params()->fromRoute('page'))
+       //         ->setItemCountPerPage(1);
+               
+//       var_dump($session->fotoPerfil); exit;
+        return new ViewModel(
+              
+        );
+    }
+    
+   
+    
+}
